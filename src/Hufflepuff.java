9545c2a2f0bea8f249da8cbb09a2798b8e721489
ibmsc <Ibm.sc1409@gmail.com>
2022-07-28 16:46:17 +0300

    Huf and Rav done

--- src/Hufflepuff.java
+++ src/Hufflepuff.java
@@ -1,5 +1,41 @@
 //Пуффендуй
-//трудолюбивы, верны, честны.
+//трудолюбивый(hardworking), верность(loyalty), честность(honesty).
 //Захария Смит, Седрик Диггори, Джастин Финч-Флетчли.
-public class Hufflepuff {
+public class Hufflepuff extends Student {
+
+    private int hardworking;
+    private int loyalty;
+    private int honesty;
+    public Hufflepuff(String name, String surname, int powerOfMagic, int distanceTransgression,
+                      int hardworking,int loyalty,int honesty)
+    {
+        super(name, surname, powerOfMagic, distanceTransgression);
+        this.hardworking = hardworking;
+        this.loyalty = loyalty;
+        this.honesty = honesty;
+    }
+
+    public int getHardworking() {
+        return hardworking;
+    }
+
+    public void setHardworking(int hardworking) {
+        this.hardworking = hardworking;
+    }
+
+    public int getLoyalty() {
+        return loyalty;
+    }
+
+    public void setLoyalty(int loyalty) {
+        this.loyalty = loyalty;
+    }
+
+    public int getHonesty() {
+        return honesty;
+    }
+
+    public void setHonesty(int honesty) {
+        this.honesty = honesty;
+    }
 }
--- src/Ravenclaw.java
+++ src/Ravenclaw.java
@@ -1,5 +1,51 @@
 //Когтевран
-// умны, мудры, остроумны и полны творчества.
+// умный(smart), мудрый(wise), остроумный(witty) и творческий(creative)
 //Чжоу Чанг, Падма Патил и Маркус Белби.
-public class Ravenclaw {
+public class Ravenclaw extends Student{
+
+    private int smart;
+    private int wise;
+    private int witty;
+    private int creative;
+    public Ravenclaw(String name, String surname, int powerOfMagic, int distanceTransgression,
+                      int smart,int wise,int witty,int creative)
+    {
+        super(name, surname, powerOfMagic, distanceTransgression);
+        this.smart = smart;
+        this.wise = wise;
+        this.witty = witty;
+        this.creative = creative;
+    }
+
+    public int getSmart() {
+        return smart;
+    }
+
+    public void setSmart(int smart) {
+        this.smart = smart;
+    }
+
+    public int getWise() {
+        return wise;
+    }
+
+    public void setWise(int wise) {
+        this.wise = wise;
+    }
+
+    public int getWitty() {
+        return witty;
+    }
+
+    public void setWitty(int witty) {
+        this.witty = witty;
+    }
+
+    public int getCreative() {
+        return creative;
+    }
+
+    public void setCreative(int creative) {
+        this.creative = creative;
+    }
 }
