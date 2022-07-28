05a65c43c96d1e27206eded2eb0da21a08596732
ibmsc <Ibm.sc1409@gmail.com>
2022-07-28 17:25:55 +0300

    Student done

--- src/Hogwarts.java
+++ src/Hogwarts.java
@@ -3,4 +3,28 @@
 //
 public class Hogwarts {
 
+    Griffindor[] griffindors = {
+            new Griffindor(" Гарри "," Поттер ",90,80,7,8,9),
+            new Griffindor(" Рон "," Грейнджер ",81,90,8,6,7),
+            new Griffindor(" Гарри "," Уизли ",76,79,8,7,6)
+    };
+    Hufflepuff[] hufflepuffs = {
+            new Hufflepuff(" Захария "," Смит ",76,72,7,8,9),
+            new Hufflepuff(" Седрик "," Диггори ",58,90,4,6,7),
+            new Hufflepuff(" Джастин "," Финч-Флетчли ",74,60,9,2,4)
+    };
+    Ravenclaw[] ravenclaws = {
+            new Ravenclaw(" Чжоу "," Чанг ",80,90,7,8,5,6),
+            new Ravenclaw(" Падма "," Патил ",80,90,8,6,5,9),
+            new Ravenclaw(" Маркус "," Белби ",69,50,7,5,8,4)
+    };
+    Slytherin[] slytherins = {
+      new Slytherin(" Драко "," Малфой ",88,79,9,5,7,
+              8,10),
+      new Slytherin(" Грэхэм "," Монтегю ",74,50,7,8,9,
+              4,7),
+      new Slytherin(" Грегори "," Гойл ",50,90,8,7,6,
+              2,5)
+    };
+
 }
